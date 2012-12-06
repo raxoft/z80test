0d3a154f558705b90e309f2b4b34a2fe1f1277c1
Patrik Rak <patrik@raxoft.cz>
2012-12-06 22:07:18 +0000

    Configure the new tests properly.

--- src/z80ccf.asm
+++ src/z80ccf.asm
@@ -4,8 +4,8 @@
             endm
 
 maskflags   equ         0
-onlyflags   equ         0
-postccf     equ         0
+onlyflags   equ         1
+postccf     equ         1
 memptr      equ         0
 
             include     main.asm
--- src/z80memptr.asm
+++ src/z80memptr.asm
@@ -4,8 +4,8 @@
             endm
 
 maskflags   equ         0
-onlyflags   equ         0
+onlyflags   equ         1
 postccf     equ         0
-memptr      equ         0
+memptr      equ         1
 
             include     main.asm
